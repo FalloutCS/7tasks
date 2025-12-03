eddaa385e0a0fa95b2c43d0936bdf62b1917c69e
FalloutCS <ruben.leopold@gmail.com>
2025-12-03 11:55:57 +0100

    Finished task 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-5xl">Welcome to my 7Tasks Challenge</h1>
+<p>
+	Visit <a href="/counter">Counter</a> to view the first project
+</p>
---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import type { PageProps } from '../counter/$types';
+
+    let { data }: PageProps = $props();
+
+    let count = $state(0)
+</script>
+
+<div class="border border-amber-500 rounded p-5 mx-auto my-auto w-1/2 xl:w-1/4 flex justify-between">
+	<label for="counter" class="border-amber-500 bg-gray-950 border rounded w-1/2 flex justify-center items-center">{count}</label>
+	<button
+		class="bg-amber-500 text-gray-900 px-3 py-1 rounded-2xl font-bold"
+		id="counter"
+		onclick={() => {
+			count++;
+		}}>Count</button
+	>
+</div>
