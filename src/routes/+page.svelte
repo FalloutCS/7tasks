5cec14dd08a0a005a3d60aae1b25c46323626cf8
FalloutCS <ruben.leopold@gmail.com>
2025-12-03 12:34:16 +0100

    small bug fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
-<h1 class="text-5xl">Welcome to my 7Tasks Challenge</h1>
-<p>
-	Visit <a href="/counter">Counter</a> to view the first project
-</p>
+<div>
+	<h1 class="text-5xl">Welcome to my 7Tasks Challenge</h1>
+	<p>
+		Visit <a class="font-bold text-amber-100" href="/counter">Counter</a> to view the first project
+	</p>
+</div>
